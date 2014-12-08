d4460a375cdcea06f9223920a69da4a5f07d0415
Moises Lopez <moylop260@vauxoo.com>
2014-12-08 21:42:29 +0000

    initial commit

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "vauxoo",
+    'website': "http://www.vauxoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
