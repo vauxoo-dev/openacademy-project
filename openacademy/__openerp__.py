e97a49a1cbc7664eb1b9d7dc64d6fc8dcde9016f
Moises Lopez <moylop260@vauxoo.com>
2014-12-10 16:05:20 +0000

    [REF] openacademy: remove description from openerp file

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -4,13 +4,6 @@
 
     'summary': """Manage trainings""",
 
-    'description': """
-        Open Academy module for managing trainings:
-            - training courses
-            - training sessions
-            - attendees registration
-    """,
-
     'author': "vauxoo",
     'website': "http://www.vauxoo.com",
 
