79976c4d370907047c9e619fc94cbdd1053132b5
Moises Lopez <moylop260@vauxoo.com>
2014-12-12 21:09:35 +0000

    REF] openacademy: Adding calendar view

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+from datetime import timedelta
+
 from openerp import api, exceptions, fields, models
 
 class Session(models.Model):
@@ -19,7 +22,8 @@ class Session(models.Model):
    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats',
                               store=True)
    active = fields.Boolean(default=True)
-
+   end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
 
    @api.one
    @api.depends('seats', 'attendee_ids')
@@ -52,5 +56,28 @@ class Session(models.Model):
        if self.instructor_id and self.instructor_id in self.attendee_ids:
             raise exceptions.ValidationError("A session's instructor can't be an attendee")
 
+   @api.one
+   @api.depends('duration', 'start_date')
+   def _get_end_date(self):
+        print "_get_end_date+++++++++++++++++"
+        if not (self.start_date and self.duration):
+            self.end_date = self.start_date
+            return
+        # Add duration to start_date, but: Monday + 5 days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+   @api.one
+   def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+        print "_set_end_date-------------------"
+        # Compute the difference between dates, but: Friday - Monday = 4 days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
