6a7d16635d7871d486792175237c9b3d42bb26f1
Moises Lopez <moylop260@vauxoo.com>
2014-12-12 21:38:05 +0000

    REF] openacademy: Adding kanban view

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -28,7 +28,7 @@ class Session(models.Model):
                          compute='_get_hours', inverse='_set_hours')
    attendees_count = fields.Integer(
         string="Attendees count", compute='_get_attendees_count', store=True)
-
+   color = fields.Integer()
 
    @api.one
    @api.depends('seats', 'attendee_ids')
