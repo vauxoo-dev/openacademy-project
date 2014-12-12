cd5e227bb144ea14f691b7b494b6b75992086c49
Moises Lopez <moylop260@vauxoo.com>
2014-12-12 18:40:19 +0000

    [REF] openacademy: Add active field and set default to fields date

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -5,7 +5,8 @@ class Session(models.Model):
    _name = 'openacademy.session'
 
    name = fields.Char(required=True)
-   start_date = fields.Date()
+   start_date = fields.Date(default=fields.Date.today)
+   datetime_eg = fields.Datetime(default=fields.Datetime.now)
    duration = fields.Float(digits=(6, 2), help="Duration in days")
    seats = fields.Integer(string="Number of seats")
    instructor_id = fields.Many2one('res.partner', string="Instructor",
@@ -17,6 +18,8 @@ class Session(models.Model):
         ondelete='cascade', string="Course", required=True)
    attendee_ids = fields.Many2many('res.partner', string="Attendees")
    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+   active = fields.Boolean(default=True)
+
 
    @api.one
    @api.depends('seats', 'attendee_ids')
