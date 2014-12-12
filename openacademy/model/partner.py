862efc8889161dc24dc2dd5f926effc32605fbd6
Moises Lopez <moylop260@vauxoo.com>
2014-12-12 17:10:21 +0000

    [REF] openacademy: Adding menu to partner

--- openacademy/model/partner.py
+++ openacademy/model/partner.py
@@ -6,6 +6,6 @@ class Partner(models.Model):
 
    instructor = fields.Boolean(default=False)
    session_ids = fields.Many2many('openacademy.session',
-                                  string="Session as instructor",
+                                  string="Session as attendee",
                                   readonly=True)
 
