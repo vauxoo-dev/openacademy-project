74413343818b45dba88af577f0a8b68b4b419df7
Moises Lopez <moylop260@vauxoo.com>
2014-12-12 21:31:28 +0000

    REF] openacademy: Adding graph view

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -26,6 +26,9 @@ class Session(models.Model):
         compute='_get_end_date', inverse='_set_end_date')
    hours = fields.Float(string="Duration in hours",
                          compute='_get_hours', inverse='_set_hours')
+   attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
+
 
    @api.one
    @api.depends('seats', 'attendee_ids')
@@ -90,6 +93,11 @@ class Session(models.Model):
    @api.one
    def _set_hours(self):
         self.duration = self.hours / 24
+
+   @api.one
+   @api.depends('attendee_ids')
+   def _get_attendees_count(self):
+      self.attendees_count = len(self.attendee_ids)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
