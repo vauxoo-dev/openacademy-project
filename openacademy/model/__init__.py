427f9098c2218455460a1079ad5c07dcb7029566
Moises Lopez <moylop260@vauxoo.com>
2014-12-12 16:56:33 +0000

    REF] openacademy: Add partner view inherit and model inheerit

--- openacademy/model/__init__.py
+++ openacademy/model/__init__.py
@@ -1,2 +1,4 @@
 from . import openacademy_course
 from . import openacademy_session
+from . import partner
+
---
+++ openacademy/model/partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models
+
+class Partner(models.Model):
+   _inherit = 'res.partner'
+
+   instructor = fields.Boolean(default=False)
+   session_ids = fields.Many2many('openacademy.session',
+                                  string="Session as instructor",
+                                  readonly=True)
+
