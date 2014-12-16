1018758c2253a8bcb3045ac401cf1f770d5f40bd
Moises Lopez <moylop260@vauxoo.com>
2014-12-16 20:29:36 +0000

    [REF] openacademy: Adding group and rule

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -22,8 +22,8 @@
         'view/openacademy_session_view.xml',
         'view/partner_view.xml',
 	'workflow/openacademy_session_workflow.xml',
-        # 'security/ir.model.access.csv',
-        #'templates.xml',
+	'security/security.xml',
+        'security/ir.model.access.csv', 
     ],
     # only loaded in demonstration mode
     'demo': [
