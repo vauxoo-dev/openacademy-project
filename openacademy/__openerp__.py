979aa8b7f1c88709f6d77a0e04ff7c616c49c0a7
Moises Lopez <moylop260@vauxoo.com>
2014-12-18 20:26:47 +0000

    [IMP] openacademy: Add board view

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -14,7 +14,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -26,6 +26,7 @@
         'security/ir.model.access.csv', 
         'view/openacademy_wizard_view.xml',
         'report/openacademy_session_report.xml',
+        'view/openacademy_session_board.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
