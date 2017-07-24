a082548d750aba05803c25bc5e75a0b85a687ff3
Moisés López <moylop260@vauxoo.com>
2017-07-24 20:08:18 +0000

    [REF] Add relation fields

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -8,3 +8,20 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one(
+        'res.users', string="Responsible",
+        index=True, ondelete='set null')
+    session_ids = fields.One2many('openacademy.session', 'course_id')
+
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string='Instructor')
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade',
+                                string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
