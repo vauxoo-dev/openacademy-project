17f36c28908f2feaa93944a991f4812e5faef8ca
Moisés López <moylop260@vauxoo.com>
2017-07-28 20:05:07 +0000

    [REF] openacademy: Add report

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -29,6 +29,7 @@
         'views/views.xml',
         'views/templates.xml',
         'views/partner.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ openacademy/report.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models
+
+
+class ReportSession(models.AbstractModel):
+    _name = "report.openacademy.report_session"
+
+    @api.multi
+    def render_html(self, data=None):
+        report_obj = self.env["report"]
+        report = report_obj._get_report_from_name("openacademy.report_session")
+        docargs = {
+            "doc_ids": self._ids,
+            "doc_model": report.model,
+            "docs": self,
+            "other_variable": 'other value',
+        }
+        return report_obj.render("openacademy.report_session", docargs)
