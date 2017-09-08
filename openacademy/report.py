0aa2b7f111a8d9b768d542fb216a807a8f54ec55
Moisés López <moylop260@vauxoo.com>
2017-09-08 18:10:15 -0500

    [FIX] openacademy: Fix report printed for 10.0

--- openacademy/report.py
+++ openacademy/report.py
@@ -6,16 +6,16 @@ from openerp import api, models
 
 
 class ReportSession(models.AbstractModel):
-    _name = "report.openacademy.report_session"
+    _name = "report.openacademy.report_session_view"
 
     @api.multi
-    def render_html(self, data=None):
+    def render_html(self, docids, data=None):
         report_obj = self.env["report"]
         report = report_obj._get_report_from_name("openacademy.report_session")
         docargs = {
-            "doc_ids": self._ids,
+            "doc_ids": docids,
             "doc_model": report.model,
-            "docs": self,
+            "docs": self.env['openacademy.session'].browse(docids),
             "other_variable": 'other value',
         }
-        return report_obj.render("openacademy.report_session", docargs)
+        return report_obj.render("openacademy.report_session_view", docargs)
